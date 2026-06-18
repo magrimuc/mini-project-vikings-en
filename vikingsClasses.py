e6322044b294f8d381ab0a15cb9e042ab0372c62
magrimuc <m.kuemmerer@pharmatechnik.de>
2026-06-18 15:12:06 +0200

    feierabend

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -71,47 +71,24 @@ class War():
         self.saxonArmy.append(saxon)
     
     def vikingAttack(self):
-        # your code here
-        pass
+        n = random.randint(0,len(self.vikingArmy)-1)
+        m = random.randint(0,len(self.saxonArmy)-1)
+        o = self.saxonArmy[m].receiveDamage(self.vikingArmy[n].strength)
+        if(self.saxonArmy[m].health < 1):
+            del self.saxonArmy[m]
+        return o
     
     def saxonAttack(self):
-        # your code here
-        pass
+        n = random.randint(0,len(self.saxonArmy)-1)
+        m = random.randint(0,len(self.vikingArmy)-1)
+        o = self.vikingArmy[m].receiveDamage(self.saxonArmy[n].strength)
+        if(self.vikingArmy[m].health < 1):
+            del self.vikingArmy[m]
+        return o
 
     def showStatus(self):
-        # your code here
-        pass
-    
-
-####
-# vikingAttack() method
-#A Saxon (chosen at random) has their receiveDamage() method called with the damage equal to the strength of a Viking (also chosen at random). This should only perform a single attack and the Saxon doesn't get to attack back.
-
-#    should be a function
-#    should receive 0 arguments
-#    should make a Saxon receiveDamage() equal to the strength of a Viking
-#    should remove dead saxons from the army
-#    should return result of calling receiveDamage() of a Saxon with the strength of a Viking
-
-#saxonAttack() method
-
-#The Saxon version of vikingAttack(). A Viking receives the damage equal to the strength of a Saxon.
-
-#    should be a function
-#    should receive 0 arguments
-#    should make a Viking receiveDamage() equal to the strength of a Saxon
-#    should remove dead vikings from the army
-#    should return result of calling receiveDamage() of a Viking with the strength of a Saxon
-
-#showStatus() method
-
-#Returns the current status of the War based on the size of the armies.
-
- #   should be a function
-  #  should receive 0 arguments
-   # if the Saxon array is empty, should return "Vikings have won the war of the century!"
-#    if the Viking array is empty, should return "Saxons have fought for their lives and survive another day..."
-#    if there are at least 1 Viking and 1 Saxon, should return "Vikings and Saxons are still in the thick of battle."
-#############
-
-
+        if(len(self.saxonArmy) == 0):
+            return "Vikings have won the war of the century!"
+        if(len(self.vikingArmy) == 0 ):
+            return "Saxons have fought for their lives and survive another day..."
+        return "Vikings and Saxons are still in the thick of battle."
